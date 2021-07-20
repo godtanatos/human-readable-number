d7301733522180aac48f26092fb4a4fb16ecebfe
godtanatos <igarcharvinsky@gmail.com>
2021-07-20 10:30:18 +0300

    feat: function  human-readable-number ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-module.exports = function toReadable (num) {
-    if(num == 0){return "zero"};
-    var a = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-        if ((num = num.toString()).length > 9) return 'overflow';
-        n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-        if (!n) return; var str = '';
-        str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + ' crore' : '';
-        str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + ' lakh' : '';
-        str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + '' + a[n[3][1]]) + ' thousand' : '';
-        str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + ' hundred' : '';
-        str += (n[5] != 0) ? ((str != '') ? ' ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + '' : '';
-if(str.charAt(str.length-1)==" "){return str.substring(0, str.length-1)}        
-return str;
-    }
-    
+module.exports = function toReadable(num) {
+    if (num == 0) { return "zero" };
+    const a = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let str = '';
+    let n = ('000' + num).substr(-3).match(/^(\d{1})(\d{2})$/);
+    /* делаю массив из строки  где отделяю сотни, тысяч, миллионы итд  /^(\d{1})(\d{1})(\d{1})(\d{2})$/*/
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + ' hundred' : '';
+    str += (n[2] != 0) ? ((str != '') ? ' ' : '') + (a[Number(n[2])] || b[n[2][0]] + (a[n[2][1]] !== '' ? ' ' + a[n[2][1]] : '')) : '';
+    return str;
+}
+
+
+
 
